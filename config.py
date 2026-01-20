6b07605347ccb420f54a9436ba50bb99e94b618f
manonlortal-sys <manonlortal@gmail.com>
2026-01-20 17:54:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,46 @@
+# config.py
 
+# Discord IDs (hardcodés)
+CHANNEL_EMBEDS_ID = 1463084740990206035
+ADMIN_ROLE_ID = 1280396795046006836
+ORGA_USER_ID = 1218696831211540522
+MATCH_CATEGORY_ID = 1326668604548186255
+
+# Emojis (unicode)
+EMOJI_INDISPO = "❌"
+EMOJI_VALIDATE = "✅"
+EMOJI_FORFAIT = "🚪"
+EMOJI_VICTORY = "🏆"
+EMOJI_THUMBS = "👍"
+
+# Classes (liste fermée)
+CLASSES = [
+    "eniripsa",
+    "cra",
+    "iop",
+    "osamodas",
+    "sram",
+    "roublard",
+    "pandawa",
+    "xelor",
+    "ecaflip",
+    "enutrof",
+    "steamer",
+    "sadida",
+    "sacrieur",
+    "feca",
+    "zobal",
+]
+
+# Maps (liste finale validée)
+MAPS = [
+    {"name": "iop", "image": "https://papycha.fr/wp-content/uploads/2023/08/iop.png"},
+    {"name": "ecaflip", "image": "https://papycha.fr/wp-content/uploads/2023/08/eca-768x541.png"},
+    {"name": "sacrieur", "image": "https://papycha.fr/wp-content/uploads/2023/08/Sacrieur.png"},
+    {"name": "sadida", "image": "https://papycha.fr/wp-content/uploads/2023/08/sadida-1.png"},
+    {"name": "eniripsa", "image": "https://papycha.fr/wp-content/uploads/2023/08/eni.png"},
+    {"name": "sram", "image": "https://papycha.fr/wp-content/uploads/2023/08/sram.png"},
+    {"name": "steamer", "image": "https://papycha.fr/wp-content/uploads/2023/08/steamer.png"},
+    {"name": "xelor", "image": "https://papycha.fr/wp-content/uploads/2023/08/map_xelor-1.png"},
+    {"name": "roublard", "image": "https://papycha.fr/wp-content/uploads/2023/08/Roublard.png"},
+]
